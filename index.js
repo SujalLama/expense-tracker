cf607aaec6669dc9bc970e2365baf8129411cb33
Sujal Lama <lsujal41@gmail.com>
2021-12-04 23:04:47 +0545

    env changed

--- index.js
+++ index.js
@@ -28,14 +28,6 @@ app.get('/', (req, res) => {
 // middleware to serve static files
 app.use(express.static(path.join(__dirname, './files')));
 
-// if(process.env.NODE_SERVER === 'production') {
-//     app.use(express.static(path.join(__dirname, "/client/build")));
-
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
-//     });
-// }
-
 const PORT = process.env.PORT || 5000;
 
 connectDB();
