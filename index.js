19073474db87fa0c8b708ad6b98f13c1c1a0c416
Sujal Lama <lsujal41@gmail.com>
2021-12-04 20:40:21 +0545

    dollar changed to rupees

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use('/api/expenses', expenseRoutes);
 app.use(express.static(path.join(__dirname, './files')));
 
 if(process.env.NODE_SERVER === 'production') {
-    app.use(express.static('../build'));
+    app.use(express.static('client/build'));
 
     // app.get('*', (req, res) => {
     //     res.sendFile(path.resolve(__dirname, '../', 'build', 'index.html'));
